1ed7f8903f9c14b23117aa63fa06db1c8817b202
Camilo Mejía <cristian.juliom@udea.edu.co>
2024-06-10 21:51:39 -0500

    Eliminando enlaces innecesarios

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -6,7 +6,7 @@ import {Session} from '@/components/home/session';
 import Image from 'next/image';
 const Navbar = () => {
   const router = useRouter();
-  const id = 1;
+  //const id = 1;
   
   return (
     <nav className='bg-white shadow dark:bg-gray-900'>
@@ -33,41 +33,7 @@ const Navbar = () => {
           </div>
           <div className='absolute inset-x-0 z-20 w-full px-6 py-4 transition-all duration-300 ease-in-out bg-white shadow-md lg:bg-transparent lg:dark:bg-transparent lg:shadow-none dark:bg-gray-900 lg:mt-0 lg:p-0 lg:top-0 lg:relative lg:w-auto lg:opacity-100 lg:translate-x-0'>
             <div className='-mx-4 lg:flex lg:items-center'>
-              <Link
-                href='/employees'
-                className='block mx-4 text-gray-700 capitalize dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400'
-              >
-                Employees
-              </Link>
-              <Link
-                  onClick={() => router.push(`/employee/${id}`)}
-                  href='Null'
-                  className='block mx-4 mt-4 text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400'
-                >
-                  Employees2
-                  {/*UI/UX Designers*/}
-                </Link>
-
-              <Link
-                href='/evaluations'
-                className='block mx-4 mt-4 text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400'
-              >
-                Evaluations
-              </Link>
-              <Link
-                href='/materials'
-                className='block mx-4 mt-4 text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400'
-              >
-                Materials
-              </Link>
-
-             {/* <Link
-                  onClick={() => router.push('http://localhost:3000/admin')}
-                  href='http://localhost:3000/admin'
-                  className='block mx-4 mt-4 text-gray-700 capitalize lg:mt-0 dark:text-gray-200 hover:text-blue-600 dark:hover:text-blue-400'
-                >
-                  Admin
-  </Link>*/}
+             
                <Link
                   onClick={() => router.push('admin')}
                   href='/admin'
