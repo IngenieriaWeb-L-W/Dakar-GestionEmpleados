2b9bb489558449578b8edd39216bd62c6f97cc31
Cristian Julio <cristian.juliom@udea.edu.co>
2024-06-11 22:49:30 -0500

    Realizando ajustes de compilación v4

--- auth.ts
+++ auth.ts
@@ -1,11 +1,20 @@
 import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
+//import prisma from "./config/prisma"
 import Auth0 from "next-auth/providers/auth0"
+
  
-const prisma = new PrismaClient()
- 
-export const { handlers, auth, signIn, signOut } = NextAuth({
+const prisma = new PrismaClient();
+export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Auth0],
+  providers: [
+    Auth0({
+      wellKnown: `https://${process.env.AUTH0_DOMAIN}/`,
+      issuer: process.env.AUTH0_DOMAIN,
+      authorization: `https://${process.env.AUTH0_DOMAIN}/authorize?response_type=code&prompt=login`,
+      clientId: `${process.env.AUTH0_CLIENT_ID}`,
+      clientSecret: `${process.env.AUTH0_CLIENT_SECRET}`,
+    }),
+],
 })
