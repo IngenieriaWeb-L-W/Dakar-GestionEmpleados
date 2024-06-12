6dee271af9dc01d55383bba7accb753c77d9d311
Cristian Julio <cristian.juliom@udea.edu.co>
2024-06-12 10:38:14 -0500

    Realizando ajustes de compilación v8

--- auth.ts
+++ auth.ts
@@ -1,12 +1,12 @@
-import NextAuth from "next-auth"
 import { PrismaAdapter } from "@next-auth/prisma-adapter"
 import { PrismaClient } from "@prisma/client"
-//import prisma from "./config/prisma"
-//import Auth0 from "next-auth/providers/auth0"
+import NextAuth from "next-auth"
+import Auth0 from "next-auth/providers/auth0"
 
+
+const prisma = new PrismaClient()
  
-const prisma = new PrismaClient();
 export const { handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [],
+  providers: [Auth0],
 })
